a0e06f2f96024bdd220e9efead518cb6f7b8e2ab
Ingrid Paiva <ingrid.paiva@ifsuldeminas.edu.br>
2018-10-29 18:41:15 -0300

    Adicionado campo email em formulario de cadastro padrão do django

--- simplemooc/simplemooc/accounts/forms.py
+++ simplemooc/simplemooc/accounts/forms.py
@@ -3,4 +3,16 @@ from django.contrib.auth.forms import UserCreationForm
 
 class RegisterForm(UserCreationForm):
 	email = forms.EmailField(label='E-mail')
-		
+
+	def clean_email(self):
+		email = self.cleaned_data['email']
+		if User.objects.filter(email=email).exists():
+			raise forms.ValidationError('Já existe usuário com este E-mail')
+		return email
+	
+	def save(self, commit=True):
+		user = super(RegisterForm, self).save(commit=False)
+		user.email = self.cleaned_data['email']
+		if commit:
+			user.save()
+		return user
--- simplemooc/simplemooc/accounts/views.py
+++ simplemooc/simplemooc/accounts/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
+
+from .forms import RegisterForm
 
 def register(request):
 	template_name = 'registration/register.html'
 
 	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
+		form = RegisterForm(request.POST)
 		if form.is_valid():
 			form.save()
 			return redirect(settings.LOGIN_URL)
 	else:
-		form = UserCreationForm()
+		form = RegisterForm()
 
 	context = {
 		'form': form
